77c32f78d7b7d4749b9102642ae1aab498ddfce4
Aparajita Singh <aparajita.1194@gmail.com>
2013-07-21 11:55:24 +0530

    Added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VotingApp::Application.config.secret_key_base = 'cef4012699039074002eb3fd9abe3a664cb6621f7fe27ede703f33cb696a7f879d2377c2f61f171d7c4b43301d0c0816e4dbf647b1d2b22ea77784d6bb5fa82c'
